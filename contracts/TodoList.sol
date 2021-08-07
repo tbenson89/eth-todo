f6722323688219019f35a2c4856128e40f2bda95
A. Tyler Benson <typrobenson@gmail.com>
2021-08-07 16:59:04 -0600

    test

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,4 +4,4 @@ pragma solidity >=0.4.22 <0.9.0;
 contract TodoList {
     // keep track of the todo list items 
     uint public taskCount = 0; // variables are written => blockchain
-}
+} // hello 
